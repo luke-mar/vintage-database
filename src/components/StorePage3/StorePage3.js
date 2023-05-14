31877f3c6a55f7130c2ea6c360a018e35dceba2b
Lucas <lukemar112@gmail.com>
2023-05-14 12:52:57 -0400

    added styling

--- src/components/StorePage3/StorePage3.js
+++ src/components/StorePage3/StorePage3.js
@@ -10,18 +10,20 @@ function StorePage3(props) {
 
   return (
     <>
+      <div className='all-prod-title'>ALL PRODUCTS FROM WONDER WHY:</div>
       {
         props.allProducts.filter(checkStore).map (product =>{
           return(
-            <div className='container'>
-              <div className='card'>
-                <img src={product.image1} className='photo' onClick={()=>{props.setNickname(product.nickname); navigate(`/item/${product.nickname}`);}}></img> 
-                <div className='row'>
-                    <div className='name'>{product.name}</div> 
-                </div>
+            <div className='card'>
+              <img src={product.image1} className='photo' onClick={()=>{props.setNickname(product.nickname); navigate(`/item/${product.nickname}`);}}></img> 
+              
+              <div className='testborder'>
+                <div className='name'>{product.name}</div> 
                 <div className='name2'>{product.size}</div> 
+
               </div>
-            </div> 
+        
+          </div>
           );
         })
       } 
